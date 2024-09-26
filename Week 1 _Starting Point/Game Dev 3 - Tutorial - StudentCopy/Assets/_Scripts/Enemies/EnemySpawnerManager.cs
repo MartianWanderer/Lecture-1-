d19b77275b5d04abaae4b3fb7c9c0dba4ea17253
Watson <R.Watson7354@student.leedsbeckett.ac.uk>
2024-09-26 12:03:18 +0100

    Class ended

---
+++ Week 1 _Starting Point/Game Dev 3 - Tutorial - StudentCopy/Assets/_Scripts/Enemies/EnemySpawnerManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnerManager : MonoBehaviour
+{
+    [SerializeField] Transform[] spawnPoints;
+    [SerializeField] float delayBetweenSpawns;
+    [SerializeField] int numberOfEnemiesSpawned;
+    [SerializeField] float delaybetweenWaves;
+    [SerializeField] 
+    [SerializeField]
+    [SerializeField]
+}
