cf7bd71b3bd8b7a62aead37e4773c1363c307caa
MartianWanderer <rawatson030@gmail.com>
2024-10-30 14:43:24 +0000

    Completed Spawn Manager Script

--- Week 1 _Starting Point/Game Dev 3 - Tutorial - StudentCopy/Assets/_Scripts/Enemies/EnemySpawnerManager.cs
+++ Week 1 _Starting Point/Game Dev 3 - Tutorial - StudentCopy/Assets/_Scripts/Enemies/EnemySpawnerManager.cs
@@ -1,12 +1,68 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawnerManager : MonoBehaviour
 {
+    //Array of available spawnPoints
     [SerializeField] Transform[] spawnPoints;
+
+    //Floats that are used as Delays
     [SerializeField] float delayBetweenSpawns;
-    [SerializeField] int numberOfEnemiesSpawned;
     [SerializeField] float delaybetweenWaves;
+
+    // Stored GameObject of enemyPrefab to spawn
+    [SerializeField] GameObject enemyPrefab;
+
+    //Array of Scriptable Objects to call from 
+    [SerializeField] EnemyData[] enemyData;
+
+    //Int to keep track of how many enemies to spawn
+    [SerializeField] int numberOfEnemiesSpawned;
+
+    //Int to set how many waves we want and private int for the current wave
+    [SerializeField] int wavesNumber;
+    private int currentWaveCount = 0;
+
+
+    public void StartWaves() 
+    {
+        StartCoroutine(SpawnWaves());
+    }
+    IEnumerator SpawnWaves()
+    {
+
+        for (int i = 0; i < numberOfEnemiesSpawned; i++)
+        {
+            // Local variable to use for "random" spawn points
+            int randomInt = Random.Range(0, spawnPoints.Length - 1); 
+
+            // Creates the ship in game at one of the positions in spawnPoints
+            GameObject spawnedShip = Instantiate(enemyPrefab, spawnPoints[randomInt]);
+
+            // changes all the variables of the spawnedShip to be equal to the variables stored on the prefab
+            spawnedShip.GetComponent<EnemyVisual>().enemyData = enemyData[currentWaveCount];
+            spawnedShip.GetComponent<EnemyMovement>().enemyData = enemyData[currentWaveCount];
+            spawnedShip.GetComponent<EnemyLife>().enemyData = enemyData[currentWaveCount];
+
+            // Delays the time before next Loop/ spawnedShip
+            yield return new WaitForSeconds(delayBetweenSpawns);
+        }
+
+        // Increases currentWaveCount by 1
+        currentWaveCount++;
+
+        // Checks if there's still more waves
+        if (currentWaveCount > enemyData.Length - 1)
+        {
+            //if there isn't then wavecount is 0 and no more loops
+            currentWaveCount = 0;
+        }
+        // More delay before next wave
+        yield return new WaitForSeconds(delaybetweenWaves);
+        
+        //Begins next wave
+        StartCoroutine(SpawnWave());
+    }
+
 
 }
